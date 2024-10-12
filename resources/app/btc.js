84dedda5816f22e2e8c6b467c8879d7b2f16b817
hartmanm <16545449+hartmanm@users.noreply.github.com>
2024-10-12 19:07:39 +0000

    cleanup

--- resources/app/btc.js
+++ resources/app/btc.js
@@ -1,59 +1,39 @@
-/***********************************************************************************************
-**
-**  Author:       Michael Hartman
-**
-**  Date:           2016-03-18
-**
-**  Filename:       btc.js
-**
-**  Description:    BTC/USD Ticker
-**
-***********************************************************************************************/
+/*
+Copyright (c) 2016 Michael Neill Hartman. All rights reserved.
+mnh_license@proton.me
+https://github.com/hartmanm
+*/
 
-    document.addEventListener( "DOMContentLoaded", getData );
-
-    document.addEventListener( "DOMContentLoaded", refresh(30000) );
-
-    function refresh( t )
-    {
-        setTimeout("location.reload(true);", t);
-    }
-
-    function getData()
-    {
-        event.preventDefault();
-
-        var req = new XMLHttpRequest();
-
-    req.open("GET", "https://api.coincap.io/v2/assets/bitcoin", false);
-
-    req.addEventListener( "load",function()
-    {
-      if( req.status >= 200 && req.status < 403 )
-      {
-
-        var response = JSON.parse( req.responseText );
-
-        var z = response.data.priceUsd;
-
-        var n = JSON.stringify( z );
-
-        n = n.slice(1, 8);
-
-		if(n.slice(1,2) != ".")
-		{
-			var n = JSON.stringify( z );
-			n = n.slice(1, 9);
-		}
-
-        document.getElementById( "btc" ).innerHTML = n;
-      }
-        
-        else
-        {
-            console.log( "Error: " + req.statusText );
-        }
-        });
-
-        req.send( null );
-    }
+document.addEventListener( "DOMContentLoaded", getData );
+document.addEventListener( "DOMContentLoaded", refresh(30000) );
+function refresh( t )
+{
+setTimeout("location.reload(true);", t);
+}
+function getData()
+{
+event.preventDefault();
+var req = new XMLHttpRequest();
+req.open("GET", "https://api.coincap.io/v2/assets/bitcoin", false);
+req.addEventListener( "load",function()
+{
+if( req.status >= 200 && req.status < 403 )
+{
+var response = JSON.parse( req.responseText );
+var z = response.data.priceUsd;
+var n = JSON.stringify( z );
+n = n.slice(1, 8);
+if(n.slice(1,2) != ".")
+{
+var n = JSON.stringify( z );
+n = n.slice(1, 9);
+}
+document.getElementById( "btc" ).innerHTML = n;
+}
+else
+{
+console.log( "Error: " + req.statusText );
+}
+});
+req.send( null );
+}
